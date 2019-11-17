ee8fbabbd821d0b24229d2f9263dc4c1f294c4df
Victor Guerra <wasmon999@hotmail.com>
2019-11-17 16:01:51 -0700

    obo

--- BinaryHeap.cpp
+++ BinaryHeap.cpp
@@ -5,6 +5,72 @@ template<typename T>
 struct nodo
 {
   T valor;
-  nodo<T>* izq = nullptr;
-  nodo<T>* der = nullptr;
+  nodo<T>* izq;
+  nodo<T>* der;
 };
+
+template<typename T>
+class BinHeap
+{
+  public:
+    void Insert(T valor)
+    {
+      if(isEmpty())
+      {
+        root = new nodo<T>;
+        root->valor = valor;
+        return;
+      }
+      else if(valor < root->valor)
+      {
+        auto next = LastNodo();
+        next->valor = valor;
+      }
+
+       
+    }
+    
+    T Take(nodo<T>* taken)
+    {
+
+    }
+
+    void Find(T buscado)
+    {
+
+    }
+
+    bool isEmpty()
+    {
+      return !root;
+    }
+
+    nodo<T>* LastNodo()
+    {
+      return nextNodo(root);
+    }
+  private:
+    nodo<T>* nextNodo(nodo<T>* actual)
+    {
+      if(!actual->izq)
+      {
+        return actual;
+      }
+      actual = nextNodo(actual->izq);
+      actual = nextNodo(actual->der);
+
+      return actual;
+    }
+
+    nodo<T>* root = nullptr;
+
+};
+
+int main()
+{
+
+  BinHeap<int> heap;
+  heap.Insert(10);
+
+}
+
